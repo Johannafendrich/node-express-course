226f9d652c2e56b9e0642186ecdabc1debc7233d
Johannafendrich <fendrichjohanna@gmail.com>
2020-03-26 10:53:11 +0100

    Fix issue

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const app = express();
 const mockUserData = [{ name: "Mark" }, { name: "Jill" }];
 
 app.get("/users", function(req, res) {
+  res.json({
+    success: true,
+    message: "successfully got users. Nice!",
+    users: mockUserData
+  });
+});
+// colons are used as variables that be viewed in the params
+app.get("/users/:id", function(req, res) {
   console.log(req.params.id);
   res.json({
     success: true,
